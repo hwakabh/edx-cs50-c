3d4e8037ea6c5a20074f43b16d9881de6620455b
hwakabh <hrykwkbys1024@gmail.com>
2019-08-11 04:36:12 +0900

    Added greedy algorithm for exchanging dollers to cents.

---
+++ pset1/greedy/greedy.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+
+const int QUARTER = 25;
+const int DIME = 10;
+const int NICKEL = 5;
+const int PENNY = 1;
+
+int main(void)
+{
+    float dollar_owed;
+    int cents_owed;
+    int coins = 0;
+
+    // TODO: Escape closed loop when user entered string(s)
+    do {
+        printf("Please enter amounts of change[$]: ");
+        scanf("%f", &dollar_owed);
+        cents_owed = round(dollar_owed * 100);
+    } while (cents_owed <= 0);
+
+    while ((cents_owed / QUARTER) != 0) {
+        coins++;
+        cents_owed -= QUARTER;
+    }
+
+    while ((cents_owed / DIME) != 0) {
+        coins++;
+        cents_owed -= DIME;
+    }
+
+    while ((cents_owed / NICKEL) != 0) {
+        coins++;
+        cents_owed -= NICKEL;
+    }
+
+    while ((cents_owed / PENNY) != 0) {
+        coins++;
+        cents_owed -= PENNY;
+    }
+
+    printf("Coins : %d\n", coins);
+    return 0;
+}
+
