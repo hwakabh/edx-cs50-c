c93e28b9878bda2edcb38ddf0a5aa3b09f5098d4
hwakabh <hrykwkbys1024@gmail.com>
2019-08-10 20:54:08 +0900

    Implement function to get digit number of user input.

--- credit/credit.c
+++ credit/credit.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 
+int get_digits_number(int number);
+
 int main(void)
 {
-    long card_num;
-    printf("Please Enter your card number : ");
-    scanf("%ld", &card_num);
-    printf("Card Number : %ld\n", card_num);
+    int user_input;
+    printf("Please enter your card number : ");
+    scanf("%d", &user_input);
+    
+    int n = get_digits_number(user_input);
+
+    int card_number[n];
+    printf("Digits of input : %d\n", n);
+    printf("Card Number : %d\n", user_input);
     return 0;
+}
+
+
+int get_digits_number(int number)
+{
+    int digit = 0;
+    while (number != 0)
+    {
+        number = number / 10;
+        digit++;
+    }
+    return digit;
 }
