d8f26e7d0047f59a0ffabdb6f439a400ea0528db
hwakabh <hrykwkbys1024@gmail.com>
2019-08-10 20:25:51 +0900

    Enabled to get user input and print them out.

---
+++ credit/credit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    long card_num;
+    printf("Please Enter your card number : ");
+    scanf("%ld", &card_num);
+    printf("Card Number : %ld\n", card_num);
+    return 0;
+}
