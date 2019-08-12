4cfff03539b5775bedb045ea9d3175fc9c0f2b45
hwakabh <hrykwkbys1024@gmail.com>
2019-08-13 00:39:50 +0900

    Implemented char calculartions.

--- pset2/caesar/caeser.c
+++ pset2/caesar/caeser.c
@@ -5,16 +5,31 @@ int main(int argc, char *argv[])
 {
     if (argc == 1) {
         printf("Please provide encryption key numbers.\n");
+        printf("Usage : ./caeser <ENCRYPT_KEY_NUMBER>\n");
         return 1;
     } else if (argc >= 3) {
         printf("Please provide just one key.\n");
         return 1;
     }
 
-    printf("Command line arguments : \n");
-    for (int i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
+    int key = 12;
+
+    char plaintext[256];
+    printf("Enter plaintext : ");
+    scanf("%s", plaintext);
+
+    printf("Ciphertext : ");
+    for (int i = 0; i < 256; i++) {
+        if (plaintext[i] == '\0') {
+            break;
+        }
+        char c = plaintext[i] + key;
+        if (c < 65 || 122 < c) {
+            printf("Out of ASCII\n");
+        }
+        printf("%c", c);
     }
+    printf("\n");
 
     return 0;
 
