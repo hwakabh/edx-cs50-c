05952b8a1169c03ecc08607f69937cd0e5241314
hwakabh <hrykwkbys1024@gmail.com>
2020-07-09 03:30:34 +0900

    Added examples for week8.

---
+++ week8/README.md
@@ -0,0 +1,3 @@
+# Week 8: Information
+
+Latest full video is available on [CS50 2020](https://cs50.harvard.edu/x/2020/weeks/8/)  
---
+++ week8/examples/password.py
@@ -0,0 +1,7 @@
+from time import sleep
+
+
+with open("./large", "r") as file:
+    for word in file.readlines():
+        print(f"Checking {word.rstrip()} ...")
+        sleep(.1)
