cd450d5b54349cb98128f18ae4e90747aedcfa9a
hwakabh <hrykwkbys1024@gmail.com>
2020-08-09 21:41:54 +0900

    Updated example with adding flask_session for app:tasks.

--- tracks/examples/flask/tasks/application.py
+++ tracks/examples/flask/tasks/application.py
@@ -1,12 +1,25 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
+# --- Session configuration, instead of global variable
+# Disabled permanent session
+app.config["SESSION_PERMANENT"] = False
+# Store session on the filesystem of webserver(note that new directory created named "./APP_ROOT/flask_sessions")
+app.config["SESSION_TYPE"] = "filesystem"
+# Append configurations to this application
+Session(app)
 
 todos = []
 
 @app.route("/")
 def tasks():
-    return render_template("tasks.html", todos=todos)
+    # For the current user, if they don't have key called `todos` inside the current user session,
+    # Create new key with their session
+    if "todos" not in session:
+        session["todos"] = []
+    # passing data stored in session key to the templates
+    return render_template("tasks.html", todos=session["todos"])
 
 # by default, Flask allows only GET
 @app.route("/add", methods=["GET", "POST"])
@@ -14,7 +27,8 @@ def add():
     if request.method == "GET":
         return render_template("add.html")
     else:
+        # Update session data only for current user
         todo = request.form.get("task")
-        todos.append(todo)
+        session["todos"].append(todo)
         return redirect("/")
 
